36be69b7c1eaa5314773975db0d2be99ba0195a8
Cjeveritt19 <callumeveritt11@gmail.com>
2025-10-03 14:26:19 +0100

    trying to fix the code to make the comments link to the posts

--- src/app/posts/[postId]/page.js
+++ src/app/posts/[postId]/page.js
@@ -2,16 +2,22 @@
 //TODO: implement a delete action to delete comments
 import { db } from "@/utlls/dbConnection";
 import Image from "next/image";
+import Form from "@/components/Form";
 
 export default async function PostIdPage({ params }) {
   const postId = (await params).postId;
 
-  const query = await db.query(
+  const postQuery = await db.query(
     `select id, title, content, author, url from posts where id = ${postId}`
   );
-  console.log(query);
+  console.log(postQuery);
 
-  const post = query.rows[0];
+  const post = postQuery.rows[0];
+  const commentsQuery = await db.query(
+    `select id, commenter, content from comments where post_id = ${postId}`
+  );
+  const comments = commentsQuery.rows;
+  console.log(comments);
 
   // console.log(post);
   return (
@@ -22,6 +28,19 @@ export default async function PostIdPage({ params }) {
         <p>{post.content}</p>
         <p>Author: {post.author}</p>
         <Image src={post?.url} alt={post.title} width={500} height={300} />
+      </div>
+      <div>
+        <h2>Comments Section</h2>
+        {comments.map((comment) => (
+          <div key={comment.id}>
+            <p>{comment.commenter}</p>
+            <p>{comment.content}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>Add a Comment</h2>
+        <Form />
       </div>
     </>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,24 +1,27 @@
 import { db } from "@/utlls/dbConnection";
 
 export default function CommentForm() {
-  async function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const comment = formData.get("comment");
+  async function handleSubmit(formData) {
+    "use server";
+    const formValues = {
+      commenter: formData.get("commenter"),
+      content: formData.get("comment"),
+    };
+    console.log(formValues);
 
-    await db.query(
-      `insert into comments (commenter, content) values ($1, $2)`,
-      [commenter, content]
-    );
+    db.query(`insert into comments (commenter, content) values ($1, $2)`, [
+      formValues.commenter,
+      formValues.content,
+    ]);
   }
 
   return (
-    <form>
+    <form action={handleSubmit}>
       <label htmlFor="commenter">Your Name:</label>
       <input type="text" name="commenter" required />
       <label htmlFor="comment">Your Comment:</label>
       <textarea id="comment" name="comment" required></textarea>
-      <button type="submit">Submit</button>
+      <button type="submit">Add Comment!</button>
     </form>
   );
 }
