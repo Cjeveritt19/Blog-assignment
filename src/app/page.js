445fe91504ff91bfa6b94e9de8e3ef43d6df4a88
Cjeveritt19 <callumeveritt11@gmail.com>
2025-10-03 17:12:10 +0100

    added styling to global css

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Guineapig from "@/../public/images/guinea-pig-on-a-laptop.jpeg";
+
 export default function HomePage() {
   return (
     <>
       <h1>Welcome to my blog</h1>
+      <Image
+        src={Guineapig}
+        alt="A guinea pig on a laptop"
+        width={500}
+        height={500}
+      />
     </>
   );
 }
